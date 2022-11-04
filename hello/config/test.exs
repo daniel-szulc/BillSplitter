6abac224101e808a33c11340e350ad00f763009a
Mati-Wojtacha <63257924+Mati-Wojtacha@users.noreply.github.com>
2022-11-04 12:04:54 +0100

    Update test.exs

--- hello/config/test.exs
+++ hello/config/test.exs
@@ -6,9 +6,9 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :hello, Hello.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
+  username: "root",
+  password: "",
+  hostname: "127.0.0.1",
   database: "hello_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
